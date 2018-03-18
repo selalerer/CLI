43cc56b05538b1d2816bb237f444a5dc292ce9c6
nirli <nir.liberman@checkmarx.com>
2018-03-18 17:22:27 +0200

    CLI example files

---
+++ cli/src/main/scripts/Examples/V870/SAST_Only.cmd
@@ -0,0 +1,11 @@
+@echo off
+
+pushd "%~dp0"
+set JAVA_HOME=
+set PATH=%JAVA_HOME%/bin;%PATH%
+
+java -Xmx1024m -jar cx_console.jar Scan -ProjectName "CxServer\CLI_SAST_SCAN" -CxServer "http://10.31.1.158" -cxUser "admin@cx" -cxPassword "XXXXX" -locationType shared -locationUser "user" -locationPassword "XXX" -locationPath "\\storage\qa\Auto_Tests\Projects\MixedSASTAndOSATest" -preset Default -v -comment "test comment" -SASTHigh 1 -SASTMedium 2 -SASTLow 3
+set exitCode=%errorlevel%
+
+popd
+Exit /B %exitCode%
