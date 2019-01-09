3f826cb9865eb0418a5878e35a973a25f4a13aa2
idanA <idan.atzmon@checkmarx.com>
2019-01-09 12:47:51 +0200

    TeamDTO update

--- cli/src/main/java/com/checkmarx/cxconsole/clients/general/dto/TeamDTO.java
+++ cli/src/main/java/com/checkmarx/cxconsole/clients/general/dto/TeamDTO.java
@@ -1,13 +1,20 @@
 package com.checkmarx.cxconsole.clients.general.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 /**
  * Created by nirli on 25/02/2018.
  */
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class TeamDTO {
 
     private String id;
 
     private String fullName;
+
+    private String name;
+
+    private String parentId;
 
     public TeamDTO() {
     }
@@ -30,5 +37,21 @@ public class TeamDTO {
 
     public void setFullName(String fullName) {
         this.fullName = fullName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(String parentId) {
+        this.parentId = parentId;
     }
 }
