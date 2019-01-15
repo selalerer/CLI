2a2063fb21656da5c88d3cbe2b8ffaada201944f
idanA <idan.atzmon@checkmarx.com>
2019-01-15 16:37:58 +0200

    sso

--- cli/src/main/java/com/checkmarx/cxconsole/clients/login/CxRestLoginClientImpl.java
+++ cli/src/main/java/com/checkmarx/cxconsole/clients/login/CxRestLoginClientImpl.java
@@ -7,10 +7,7 @@ import com.checkmarx.cxconsole.clients.login.utils.LoginResourceURIBuilder;
 import com.checkmarx.cxconsole.clients.token.utils.TokenHttpEntityBuilder;
 import com.checkmarx.cxconsole.clients.utils.RestClientUtils;
 import com.google.common.base.Strings;
-import org.apache.http.Header;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
+import org.apache.http.*;
 import org.apache.http.auth.AuthSchemeProvider;
 import org.apache.http.client.CookieStore;
 import org.apache.http.client.CredentialsProvider;
@@ -21,10 +18,10 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.client.methods.RequestBuilder;
 import org.apache.http.client.utils.HttpClientUtils;
+import org.apache.http.client.utils.URLEncodedUtils;
 import org.apache.http.config.Registry;
 import org.apache.http.config.RegistryBuilder;
 import org.apache.http.cookie.Cookie;
-import org.apache.http.entity.BasicHttpEntity;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.auth.BasicSchemeFactory;
@@ -34,19 +31,18 @@ import org.apache.http.impl.auth.win.WindowsNTLMSchemeFactory;
 import org.apache.http.impl.auth.win.WindowsNegotiateSchemeFactory;
 import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.client.LaxRedirectStrategy;
 import org.apache.http.impl.client.SystemDefaultCredentialsProvider;
 import org.apache.http.message.BasicHeader;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.protocol.HTTP;
 import org.apache.http.ssl.SSLContextBuilder;
-import org.apache.http.util.EntityUtils;
 import org.apache.log4j.Logger;
 
 import javax.net.ssl.SSLContext;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
@@ -96,7 +92,7 @@ public class CxRestLoginClientImpl implements CxRestLoginClient {
                     .create()
                     .setDefaultHeaders(headers)
                     .setSSLContext(sslContext)
-                    .setRedirectStrategy(new LaxRedirectStrategy())
+//                    .setRedirectStrategy(new LaxRedirectStrategy())
                     .build();
         } catch (CxRestLoginClientException e) {
             if (e.getMessage().contains(SERVER_STACK_TRACE_ERROR_MESSAGE)) {
@@ -140,7 +136,6 @@ public class CxRestLoginClientImpl implements CxRestLoginClient {
                 .setDefaultAuthSchemeRegistry(authSchemeRegistry)
                 .setDefaultCookieStore(cookieStore)
                 .setDefaultHeaders(headers)
-                .setRedirectStrategy(new LaxRedirectStrategy())
                 .setSSLContext(sslContext)
                 .build();
     }
@@ -186,36 +181,64 @@ public class CxRestLoginClientImpl implements CxRestLoginClient {
     public void ssoLogin() throws CxRestLoginClientException {
         HttpUriRequest request;
         HttpResponse loginResponse = null;
+        final String BASE_URL = "/CxRestAPI/auth/identity/";
+        RequestConfig requestConfig = RequestConfig.custom()
+                .setRedirectsEnabled(false)
+                .setAuthenticationEnabled(true)
+                .build();
+
         try {
+            //Request1
             request = RequestBuilder.post()
                     .setUri(String.valueOf(LoginResourceURIBuilder.buildWindowsAuthenticationLoginURL(new URL(hostName))))
-                    .setConfig(RequestConfig.DEFAULT)
-                    .setHeader("Content-Type", ContentType.APPLICATION_FORM_URLENCODED.toString())
+                    .setConfig(requestConfig)
+                    .setHeader(HttpHeaders.CONTENT_TYPE, ContentType.APPLICATION_FORM_URLENCODED.toString())
                     .setEntity(generateEntity())
                     .build();
             loginResponse = client.execute(request);
 
-            RestClientUtils.validateClientResponse(loginResponse, 200, "Fail to authenticate");
+            //Request2
+            String cookies = retrieveCookies();
+            String redirectURL = loginResponse.getHeaders("Location")[0].getValue();
+            request = RequestBuilder.get()
+                    .setUri(hostName + BASE_URL + redirectURL)
+                    .setConfig(requestConfig)
+                    .setHeader("Cookie", cookies)
+                    .setHeader("Upgrade-Insecure-Requests", "1")
+                    .build();
+            loginResponse = client.execute(request);
+
+            //Request3
+            cookies = retrieveCookies();
+            redirectURL = loginResponse.getHeaders("Location")[0].getValue();
+            request = RequestBuilder.get()
+                    .setUri(hostName + redirectURL)
+                    .setConfig(requestConfig)
+                    .setHeader(HttpHeaders.CONTENT_TYPE, ContentType.APPLICATION_FORM_URLENCODED.toString())
+                    .setHeader("Cookie", cookies)
+                    .build();
+            loginResponse = client.execute(request);
+
+            // RestClientUtils.validateClientResponse(loginResponse, 200, "Fail to authenticate");
             RestGetAccessTokenDTO jsonResponse = RestClientUtils.parseJsonFromResponse(loginResponse, RestGetAccessTokenDTO.class);
-            headers.add(new BasicHeader("Authorization", "Bearer " + jsonResponse.getAccessToken()));
-        } catch (IOException | CxValidateResponseException e) {
+            CxRestLoginClientImpl.headers.add(new BasicHeader("Authorization", "Bearer " + jsonResponse.getAccessToken()));
+        } catch (IOException e) {
             log.error("Fail to login with windows authentication: " + e.getMessage());
             throw new CxRestLoginClientException("Fail to login with windows authentication: " + e.getMessage());
         } finally {
             HttpClientUtils.closeQuietly(loginResponse);
         }
         isLoggedIn = true;
-//        for (Cookie cookie : cookieStore.getCookies()) {
-//            if (cookie.getName().equals(CSRF_TOKEN_HEADER))
-//                csrfToken = cookie.getValue();
-//            }
-//            if (cookie.getName().equals(CX_COOKIE)) {
-//                cxCookie = cookie.getValue();
-//            }
-//        }
-//
-//        headers.add(new BasicHeader(CSRF_TOKEN_HEADER, csrfToken));
-//        headers.add(new BasicHeader("cookie", String.format("CXCSRFToken=%s; cxCookie=%s", csrfToken, cxCookie)));
+    }
+
+    private String retrieveCookies() {
+        List<Cookie> cookieList = cookieStore.getCookies();
+        String cookies = "";
+        for (Cookie cookie : cookieList) {
+            cookies += cookie.getName() + "=" + cookie.getValue() + ";";
+        }
+
+        return cookies;
     }
 
     private void getAccessTokenFromRefreshToken(String refreshToken) throws CxRestLoginClientException {
@@ -280,11 +303,30 @@ public class CxRestLoginClientImpl implements CxRestLoginClient {
     }
 
     private StringEntity generateEntity() throws CxRestLoginClientException {
+//        final String BASE_URL = "/CxRestAPI/auth/identity/";
+
+        //String redirectURL = hostName + BASE_URL + String.format("connect/authorize/callback?client_id=%s&redirect_uri=%s&response_type=%s&scope=%s", "cli_client", hostName + "/CxWebClient/authCallback.html?", "token", "sast_api%20openid%20offline_access%20management_and_orchestration_api");
+//        final String redirectURL = "/CxRestAPI/auth/identity/connect/authorize/callback?client_id=cli_client&redirect_uri=http%3A%2F%2F10.31.2.9%2Fcxwebclient%2FauthCallback.html%3F&response_type=id_token%20token&scope=scope=openid%20offline_access%20management_and_orchestration_api";
+//                "connect/authorize/callback" +
+//                "?client_id=cli_client" +
+//                "&redirect_uri=http%3A%2F%2F10.31.2.9%2Fcxwebclient%2FauthCallback.html%3F&response_type=id_token%20token" +
+//                "&scope=sast_api%20openid%20offline_access%20management_and_orchestration_api";
+//        urlParameters.add(new BasicNameValuePair("redirectUrl", String.format("%s/CxRestAPI/auth/", hostName)));
+//        urlParameters.add(new BasicNameValuePair("redirectUrl", String.format("%s/CxWebClient/authCallback.html?", hostName)));
+
+        String redirectUrl = "%2FCxRestAPI%2Fauth%2Fidentity%2Fconnect%2Fauthorize%2Fcallback%3Fclient_id%3Dcli_client%26redirect_uri%3Dhttp%253A%252F%252F10.31.2.9%252Fcxwebclient%252FauthCallback.html%253F%26response_type%3Did_token%2520token%26scope%3Dsast_api%2520openid%2520sast-permissions%2520access-control-permissions%2520access_control_api%2520management_and_orchestration_api&providerid=2";
+
         List<NameValuePair> urlParameters = new ArrayList<>();
-        urlParameters.add(new BasicNameValuePair("redirectUrl", String.format("%s/CxWebClient/authCallback.html?", hostName)));
-        urlParameters.add(new BasicNameValuePair("providerid", "2"));
+//        urlParameters.add(new BasicNameValuePair("redirectUrl", "/CxRestAPI/auth/identity/connect/authorize/callback?" +
+//                "client_id=cli_client" +
+//                "&redirect_uri=" + hostName + "/cxwebclient/authCallback.html?" +
+//                "&scope=sast_api%20openid%20offline_access%20management_and_orchestration_api" +
+//                "&esponse_type=id_token token"));
+//                "&providerid=2"));
 
         try {
+            urlParameters.add(new BasicNameValuePair("redirectUrl", redirectUrl));
+            urlParameters.add(new BasicNameValuePair("providerid", "2"));
             return new UrlEncodedFormEntity(urlParameters, StandardCharsets.UTF_8.name());
         } catch (UnsupportedEncodingException e) {
             throw new CxRestLoginClientException(e.getMessage());
